8278e172de852cb382443d4db74c7982cd71040d
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2023-09-14 00:20:54 +0600

    add configuration file

---
+++ assets/configuration.js
@@ -0,0 +1,20 @@
+/**
+ * *Title: configuration file
+ * ?description: configuration properties for the application component
+ * @author: Faruk_sarkar
+ * *Date: 13-September-2023
+ */
+
+// module exports
+
+// application scaffolding configuration
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = {
+	dbUri: isProduction ? 'localhost' : process.env.DB_HOST,
+	server: isProduction ? process.env.NODE_ENV : 'development',
+	port: isProduction ? 5000 : process.env.SERVER_PORT,
+};
+
+// export this scaffold
+module.exports = app;
