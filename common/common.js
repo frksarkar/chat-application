8740b0698a213772ac5c62e038cb575ab602b7fc
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2023-09-15 20:47:48 +0600

    some change

--- common/common.js
+++ common/common.js
@@ -5,19 +5,16 @@
  * *Date: 14-September-2023
  */
 
+// module
+const env = require('../assets/configuration');
+
 function notFoundHandler(req, res, next) {
-	res.json({ message: 'page not found' });
+	res.locals.title = 'page not found';
+	res.render('error');
 }
 
 function errorHandler(err, req, res, next) {
-	// console.loo('error name:', err.name);
-	// console.loo('error message:', err.message);
-	// console.loo('error stack', err.stack);
-	// console.loo('error status code:', err.statusCode);
-
-	res.json({
-		message: err.message,
-	});
+	res.render('error');
 }
 
 module.exports = {
