a65ba003804aa7f61d495f780dc640117223d939
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2023-09-22 00:53:01 +0600

    password hash

--- router/userHandler.js
+++ router/userHandler.js
@@ -3,23 +3,38 @@ const express = require('express');
 
 // internal module imports
 const getUser = require('../controller/usersController');
+// const config = require('../assets/configuration');
 const decorateHtmlResponse = require('../common/decorateHtmlResponse');
+const bcrypt = require('bcrypt');
 const getAvatar = require('../controller/avatarController');
 const {
 	validationField,
 	userValidation,
 } = require('../common/validationField');
+const User = require('../model/userSchema');
 const router = express.Router();
 
 router.get('/', decorateHtmlResponse('User'), getUser);
 
-router.post('/', getAvatar, validationField, userValidation, (req, res) => {
-	// console.log(req.body);
-	res.json({
-		massage: {
-			fileUploaded: 'file upload successfully',
-		},
-	});
-});
+router.post(
+	'/',
+	getAvatar,
+	validationField,
+	userValidation,
+	async (req, res) => {
+		const user = { ...req.body };
+		const bcryptPassword = await bcrypt.hash(user.password, 10);
+		user.password = bcryptPassword;
+		const newUser = new User({ ...user, avatar: req.files.filename });
+		console.log(newUser);
+		const userInfo = await newUser.save();
+		res.json({
+			massage: {
+				userInformation: userInfo,
+				fileUploaded: 'file upload successfully',
+			},
+		});
+	}
+);
 
 module.exports = router;
