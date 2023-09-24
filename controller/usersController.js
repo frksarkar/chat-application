7020164599e8fea605dd19b7a9bf1069fcd515b0
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2023-09-25 02:05:47 +0600

    add remove user function

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,5 +1,37 @@
-function getUser(req, res) {
-	res.render('users');
+const Users = require('../model/userSchema');
+const { unlink } = require('fs');
+
+async function getUser(req, res, next) {
+	try {
+		const users = await Users.find();
+		res.render('users', { users });
+	} catch (error) {
+		next(error);
+	}
 }
 
-module.exports = getUser;
+// remove user
+async function removeUser(req, res, next) {
+	try {
+		const user = await Users.findByIdAndDelete({ _id: req.params.id });
+		if (user.avatar) {
+			const dirLink = `${__dirname}/../public/avatar/${user.avatar}`;
+			unlink(dirLink, (err) => {
+				if (err) {
+					throw err;
+				}
+			});
+		}
+		res.status(200).json({
+			message: 'user was removed successfully',
+		});
+	} catch (error) {
+		res.status(500).json({
+			error: {
+				common: 'Could not remove user!',
+			},
+		});
+	}
+}
+
+module.exports = { getUser, removeUser };
