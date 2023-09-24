71c9cd4c61e62fc53703ac2f7aa6a62e2bf71589
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2023-09-25 02:06:05 +0600

    some change

--- controller/user/addUser.js
+++ controller/user/addUser.js
@@ -3,14 +3,17 @@ const User = require('../../model/userSchema');
 const config = require('../../assets/configuration');
 
 const addUser = async (req, res) => {
-	const user = { ...req.body };
+	const user = {
+		...req.body,
+		avatar: req.files[0] ? req.files[0].filename : undefined,
+	};
 	const bcryptPassword = await bcrypt.hash(user.password, config.saltRounds);
 	user.password = bcryptPassword;
-	const newUser = new User({ ...user, avatar: req.files.filename });
-	console.log(newUser);
-	const userInfo = await newUser.save();
+	const newUser = new User(user);
+	await newUser.save();
+	delete user.password;
 	res.json({
-		userInfo: userInfo,
+		userInfo: user,
 		massage: 'file upload successfully',
 	});
 };
