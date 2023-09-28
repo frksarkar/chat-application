335e7852c161b204f163b832748d8793ddd5bff9
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2023-09-28 20:39:13 +0600

    add jwt & cookie config

--- assets/configuration.js
+++ assets/configuration.js
@@ -11,12 +11,16 @@ require('dotenv').config();
 // application scaffolding configuration
 const isProduction = process.env.NODE_ENV === 'production';
 
-const app = {
+const config = {
 	dbUri: isProduction ? 'localhost' : process.env.DB_HOST,
 	server: isProduction ? process.env.NODE_ENV : 'development',
 	port: isProduction ? 5000 : process.env.SERVER_PORT,
 	saltRounds: Number(process.env.SALT_ROUNDS),
+	jwtSecretKey: process.env.JWT_SECRET_KEY,
+	jwtExpireSecond: process.env.JWT_EXPIRE_SECONDS,
+	cookieName: process.env.COOKIE_NAME,
+	cookieSecretKey: process.env.COOKIE_SECRET_KEY,
 };
 
 // export this scaffold
-module.exports = app;
+module.exports = config;
