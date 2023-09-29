4046b6865e921a5268f5605048b9e5ca06e666d8
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2023-09-30 03:14:12 +0600

    login validation functionality

---
+++ middleware/login/loginFieldValidation.js
@@ -0,0 +1,32 @@
+const { check, validationResult } = require('express-validator');
+const config = require('../../assets/configuration');
+
+const fieldValidations = [
+	check('username')
+		.trim()
+		.isLength({ min: 3 })
+		.withMessage('Username must be at least 3 characters long'),
+
+	check('password')
+		.isStrongPassword()
+		.withMessage('password must be at least 8 characters long'),
+];
+
+const validationCheck = function (req, res, next) {
+	res.locals.username = req.body.username;
+	const result = validationResult(req);
+	const error = result.mapped();
+	if (Object.keys(error).length === 0) {
+		//
+		next();
+	} else {
+		res.render('index', {
+			error: error,
+		});
+	}
+};
+
+module.exports = {
+	fieldValidations,
+	validationCheck,
+};
