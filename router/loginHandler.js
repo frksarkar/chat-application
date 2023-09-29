e800d5da94fe9fb0d4765f7f111d3889cfe41818
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2023-09-30 03:15:06 +0600

    login handler

--- router/loginHandler.js
+++ router/loginHandler.js
@@ -2,11 +2,24 @@
 const express = require('express');
 
 // external module import
-const getLogin = require('../controller/loginController');
-const decorateHtmlResponse = require('../common/decorateHtmlResponse');
+const { getLogin, login } = require('../controller/loginController');
+const decorateHtmlResponse = require('../middleware/common/decorateHtmlResponse');
+const {
+	validationCheck,
+	fieldValidations,
+} = require('../middleware/login/loginFieldValidation');
+const { redirectLoggedIn } = require('../middleware/login/checkLogin');
 
 const router = express.Router();
 
-router.get('/', decorateHtmlResponse('login'), getLogin);
+router.get('/', decorateHtmlResponse('login'), redirectLoggedIn, getLogin);
+
+router.post(
+	'/',
+	decorateHtmlResponse('login'),
+	fieldValidations,
+	validationCheck,
+	login
+);
 
 module.exports = router;
