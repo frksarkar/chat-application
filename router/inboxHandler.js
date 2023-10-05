8798ba5e7b36f25f95e723998f5d1a5e0dd234ee
Omar Faruk Sarkar <hazrat.omurfaruk@gmail.com>
2023-10-05 12:48:58 +0600

    conversation functionality

--- router/inboxHandler.js
+++ router/inboxHandler.js
@@ -2,12 +2,20 @@
 const express = require('express');
 
 // internal module import
-const getInbox = require('../controller/inboxController');
+const {
+	getInbox,
+	searchUser,
+	addConversation,
+} = require('../controller/inboxController');
 const decorateHtmlResponse = require('../middleware/common/decorateHtmlResponse');
 const { checkLogin } = require('../middleware/login/checkLogin');
 const router = express.Router();
 
 // index page
 router.get('/', decorateHtmlResponse('inbox'), checkLogin, getInbox);
+
+router.post('/search', checkLogin, searchUser);
+
+router.post('/conversation', checkLogin, addConversation);
 
 module.exports = router;
